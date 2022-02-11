36dbf30140617de40e387c177b1f8ff0f572f8a8
alphadzingina <alphaadx6@gmail.com>
2022-02-11 03:10:33 +0100

    add color to shapes

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import turtle as t
+import random
 
 tim = t.Turtle()
+
+colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
 
 def draw_shape(num_sides):
     angle = 360 / num_sides
@@ -9,4 +12,5 @@ def draw_shape(num_sides):
         tim.right(angle)
 
 for shape_side_n in range(3, 10):
+    tim.color(random.choice(colours))
     draw_shape(shape_side_n)
